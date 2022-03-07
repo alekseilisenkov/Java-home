8c07513ae6ede31649b1a84c86e1e4f423f24952
alekseilisenkov <alexlisenkov92@mail.ru>
2022-03-08 00:28:16 +0300

    Final commit

--- src/main/java/com/simbirsoft/Main.java
+++ src/main/java/com/simbirsoft/Main.java
@@ -6,17 +6,42 @@ public class Main {
 
 
     public static void main(String[] args) {
-       Car nissan = new Car("green", 180, 12, "almera", false );
-       Car toyota = new Car("black", 200, 15, "crown", true);
+       Car nissan = new Car("green", 180, 12, "almera", false); //переменная это конкретное хранение данных внутри метода
+       Car avtovas = new Car("green", 120, 11, "largus", false);
+       Car toyota = new Car("black", 15, 200, "crown", true);
+
+       boolean safetyOfNissan = nissan.isCarSafe(); //переменная для метода
+       boolean safetyOfAvtovas = avtovas.isCarSafe();
+       boolean safetyOfToyota = toyota.isCarSafe();
+
+        nissan.checkForBeaty();
+        toyota.checkForBeaty();
+        avtovas.checkForBeaty();
+
+
+       nissan.speedLimit(200); //После точки вызывается метод конкретного объекта, который лежит в переменной
+       toyota.speedLimit(200);
+       avtovas.speedLimit(200);
+       System.out.println("Максимально допустимая законом скорость для" + " nissan " + nissan.maxAvaliableSpeed);
+        System.out.println("Максимально допустимая законом скорость для" + " toyota " + toyota.maxAvaliableSpeed);
+        System.out.println("Максимально допустимая законом скорость для" + " avtovas " + avtovas.maxAvaliableSpeed);
+
+        nissan.checkForFuel(12);
+        toyota.checkForFuel(15);
+        avtovas.checkForFuel(9);
+
+//        "".substring()
 
 
 
-       nissan.isCarSafe(false);
-       nissan.checkForBeauty("");
-       toyota.isCarSafe(true);
-       toyota.checkForBeauty("black");
 
-        System.out.println("Ниссан это безопасная машина");
+
+//       nissan.isCarSafe(false);
+//       nissan.checkForBeauty("green");
+//       toyota.isCarSafe(true);
+//       toyota.checkForBeauty("black");
+
+
 
     }
 
