911c799cbe82c338fb94ce9a62d0271ef5066869
alekseilisenkov <alexlisenkov92@mail.ru>
2022-06-06 19:31:31 +0300

    refactor of class car and main

---
+++ src/main/java/com/alexlisenkov/Car.java
@@ -0,0 +1,63 @@
+package com.alexlisenkov;
+
+public class Car {
+    String color;
+    int maxSpeed;
+    int fuelConsumption;
+    String brand;
+    boolean isSafeCar;
+    int maxAvaliableSpeed;
+    String[] parameters = {"weight", "height", "length", "width"};
+
+    public Car(String color, int maxSpeed, int fuelConsumption, String brand, boolean isSafeCar) {
+        this.color = color;
+        this.maxSpeed = maxSpeed;
+        this.fuelConsumption = fuelConsumption;
+        this.brand = brand;
+        this.isSafeCar = isSafeCar;
+    }
+
+    public Car() {
+    }
+
+    void speedLimit(int limit) {
+        this.maxAvaliableSpeed = limit;
+    }
+
+    boolean isCarSafe() {
+        if (isSafeCar) {
+            System.out.println("Машина безопасна");
+        } else {
+            System.out.println("Машина не безопасна");
+        }
+        return isSafeCar;
+    }
+
+    Car checkForBeaty() {
+        System.out.println("Эта машина:");
+        if (color == "green") {
+            System.out.println("Красивая");
+        } else {
+            System.out.println("Не красивая");
+        }
+        return this;
+    }
+
+    void checkForFuel(int count1) {
+        System.out.println("Расход машины:");
+        if (fuelConsumption >= 15) {
+            System.out.println("Довольно большой");
+        } else if (fuelConsumption < 15 && fuelConsumption >= 10) {
+            System.out.println("Средний");
+        } else if (fuelConsumption < 10) {
+            System.out.println("Экономичный");
+        }
+    }
+
+}
+
+
+
+
+
+
---
+++ src/main/java/com/alexlisenkov/Main.java
@@ -0,0 +1,35 @@
+package com.alexlisenkov;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+        Car nissan = new Car("green", 180, 12, "almera", false);
+        Car avtovas = new Car("green", 120, 11, "largus", false);
+        Car toyota = new Car("black", 15, 200, "crown", true);
+
+        boolean safetyOfNissan = nissan.isCarSafe();
+        boolean safetyOfAvtovas = avtovas.isCarSafe();
+        boolean safetyOfToyota = toyota.isCarSafe();
+
+        nissan.checkForBeaty();
+        toyota.checkForBeaty();
+        avtovas.checkForBeaty();
+
+
+        nissan.speedLimit(200);
+        toyota.speedLimit(200);
+        avtovas.speedLimit(200);
+        System.out.println("Максимально допустимая законом скорость для" + " nissan " + nissan.maxAvaliableSpeed);
+        System.out.println("Максимально допустимая законом скорость для" + " toyota " + toyota.maxAvaliableSpeed);
+        System.out.println("Максимально допустимая законом скорость для" + " avtovas " + avtovas.maxAvaliableSpeed);
+
+        nissan.checkForFuel(12);
+        toyota.checkForFuel(15);
+        avtovas.checkForFuel(9);
+
+
+    }
+
+}
+
